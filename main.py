cea3ce85c904dbc562b65736f160329b4da37d38
CJ <weicongju@gmail.com>
2012-12-04 23:30:52 +0800

    mail error

--- main.py
+++ main.py
@@ -76,34 +76,41 @@ class CMD:
 
 def deploy ( branch, setting ):
 
-	repos_path = os.path.join ( REPOSITORY_DIR, "post_bar" )
+	def _deploy ( branch, setting ):
 
-	clone = False
-	cmd = CMD ( )
-	cmd.open ( "rm -rf %s" % repos_path )
+		repos_path = os.path.join ( REPOSITORY_DIR, "post_bar" )
+
+		clone = False
+		cmd = CMD ( )
+		cmd.open ( "rm -rf %s" % repos_path )
 
 
-	if not os.path.exists ( repos_path ):
-		os.chdir ( REPOSITORY_DIR )
-		pin_fd, pout_fd, perr_fd = os.popen3 ( "git clone http://github.com/shitou/post_bar"  )
-		clone = True
-	
-	cmd.open ( "find %s" % setting['application_root'] )
-	mail ( cmd.allDescription ( ) )
-	os.chdir ( repos_path )
-	
-	cmd = CMD ( )
-	cmd.open ( "git pull"  )
-	cmd.open ( "git checkout %s" % ( branch ) )
-	# cmd.open ( "gem install af" )
-	cmd.open ( "af login %(ap_email)s --passwd %(ap_passwd)s" % setting  )
-	cmd.open ( "af update %(ap_app)s" % setting  )
-	cmd.open ( "af logs %(ap_app)s" % setting  )
-	# cmd.open ( "ruby %(application_root)s/af/bin/af login %(ap_email)s --passwd %(ap_passwd)s" % setting  )
-	# cmd.open ( "ruby %(application_root)s/af/bin/af update %(ap_app)s" % setting  )
-	# cmd.open ( "ruby %(application_root)s/af/bin/af logs %(ap_app)s" % setting  )
+		if not os.path.exists ( repos_path ):
+			os.chdir ( REPOSITORY_DIR )
+			pin_fd, pout_fd, perr_fd = os.popen3 ( "git clone http://github.com/shitou/post_bar"  )
+			clone = True
+		
+		cmd.open ( "find %s" % setting['application_root'] )
+		mail ( cmd.allDescription ( ) )
+		os.chdir ( repos_path )
+		
+		cmd = CMD ( )
+		cmd.open ( "git pull"  )
+		cmd.open ( "git checkout %s" % ( branch ) )
+		# cmd.open ( "gem install af" )
+		cmd.open ( "af login %(ap_email)s --passwd %(ap_passwd)s" % setting  )
+		cmd.open ( "af update %(ap_app)s" % setting  )
+		cmd.open ( "af logs %(ap_app)s" % setting  )
+		# cmd.open ( "ruby %(application_root)s/af/bin/af login %(ap_email)s --passwd %(ap_passwd)s" % setting  )
+		# cmd.open ( "ruby %(application_root)s/af/bin/af update %(ap_app)s" % setting  )
+		# cmd.open ( "ruby %(application_root)s/af/bin/af logs %(ap_app)s" % setting  )
 
-	mail ( cmd.allDescription ( ) )
+		mail ( cmd.allDescription ( ) )
+
+	try:
+		_deploy ( branch, setting )
+	except Exception, e:
+		mail ( e )
 
 def mail ( content ):
 	client = smtplib.SMTP_SSL ( setting['smtp_host'] )
